e8654a175a0abc81b6ed283eaf929da07450a0ba
Nikita Kuznetsov <nkuznetsov825@gmail.com>
2020-10-23 15:40:13 +0200

    Changed adding channels model

--- FintechChatApp/ConversationListServerManager.swift
+++ FintechChatApp/ConversationListServerManager.swift
@@ -72,7 +72,7 @@ class ConversationListServerManager {
         reference.addDocument(data: [
             "name": channel.name,
             "lastMessage": channel.lastMessage as String? as Any,
-            "lastActivity": Timestamp()
+            "lastActivity": channel.lastActivity as Date? as Any
         ])
     }
     
