0a4759f92bf4b7921e00243d137af4398d9de3bc
Nikita Kuznetsov <nkuznetsov825@gmail.com>
2020-10-23 23:38:15 +0200

    Created Chat Data Model and CoreDataStack class

---
+++ FintechChatApp/CoreDataStack.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataStack.swift
+//  FintechChatApp
+//
+//  Created by Никита Кузнецов on 23.10.2020.
+//  Copyright © 2020 dreamTeam. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+}
